60c000e3e22f28de59772141596fa715164c7107
HorangApple <horangapple@gmail.com>
2023-05-29 00:16:11 +0900

    상품 선택과 삭제를 주문에서 보여주기

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
-import Prototypes from "./components/Prototypes";
+import Prototypes from "./Prototypes";
 import Orders from "./components/Orders";
 import Footer from "./components/Footer";
+import AppStateProvider from "./providers/AppStateProvider";
 
 function App() {
   return (
-    <>
+    <AppStateProvider>
       <Header />
       <div className={"container"}>
         <Prototypes />
         <Orders />
         <Footer />
       </div>
-    </>
+    </AppStateProvider>
   );
 }
 
